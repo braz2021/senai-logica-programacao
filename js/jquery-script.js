3bab26dfbae38cb0ec87f652df6150eee286c404
BRAZ DINIZ <brazdiniz014@gmail.com>
2023-11-07 23:33:08 -0300

    site

---
+++ js/jquery-script.js
@@ -0,0 +1,11 @@
+//efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+    
+    $("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slideToglle("slow");
+        $("#form-cadastrar").slideToglle("slow");
+        $("#botao-cadastrar").hide();
+    });
+})
